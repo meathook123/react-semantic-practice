79cdd669f2dbd3022511778f8b4ec4de0ba29b36
Ruiyang Sun <sunruiyang09@gmail.com>
2017-07-25 17:01:42 -0700

    change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+import Topnav from './global/topnav/Topnav';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Topnav />
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
