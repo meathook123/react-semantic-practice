3ba5e64ce634d53b89fae6d40d47b5f956f34fbe
Ruiyang Sun <sunruiyang09@gmail.com>
2017-07-25 19:00:38 -0700

    add semantic sidebar

--- src/global/topnav/Topnav.js
+++ src/global/topnav/Topnav.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './Topnav.css';
+import { Button, Icon } from 'semantic-ui-react'
 
 class Topnav extends Component {
   render() {
     return (
-      <div>
-        <p>navnav</p>
-      </div>
+      <nav>
+        <Icon name='list layout' onClick={this.props.buttonClick} className="nav-icon"/>
+        <span className="nav-title">Sample</span>
+      </nav>
     );
   }
 }
 
 export default Topnav;
+// <img src='/img/logo.png' alt="" onClick={this.props.buttonClick} className="nav-icon"/>
--- src/index.js
+++ src/index.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 import Topnav from './global/topnav/Topnav';
+import { Sidebar, Segment, Menu, Icon } from 'semantic-ui-react'
 
 class App extends Component {
+  state = { visible: false }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
   render() {
+    const { visible } = this.state
     return (
-      <div className="App">
-        <Topnav />
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar as={Menu} animation='slide along' width='thin' visible={visible} icon='labeled' vertical inverted>
+          <Menu.Item name='suitcase'>
+            <Icon name='suitcase' />
+            Work
+          </Menu.Item>
+          <Menu.Item name='paint brush'>
+            <Icon name='paint brush' />
+            Gallery
+          </Menu.Item>
+          <Menu.Item name='smile'>
+            <Icon name='smile' />
+            About
+          </Menu.Item>
+        </Sidebar>
+        <Sidebar.Pusher >
+          <Segment basic>
+            <div >
+              <Topnav buttonClick={this.toggleVisibility.bind(this)}></Topnav>
+            </div>
+          </Segment>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
     );
   }
 }
